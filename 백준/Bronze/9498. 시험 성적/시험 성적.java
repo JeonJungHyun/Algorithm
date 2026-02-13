b2eab25015f000a54ddbad71daa183243e8840bb
JungHyun <3847155@gmail.com>
2026-02-13 16:52:19 +0900

    [Bronze V] Title: 시험 성적, Time: 104 ms, Memory: 14168 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader( new InputStreamReader( System.in ) );
+        int a = Integer.parseInt( br.readLine() );
+        if( a >= 90 && a <= 100 ) {
+            System.out.println( "A" );
+        } else if( a >= 80 && a <= 89 ) {
+            System.out.println( "B" );
+        } else if( a >= 70 && a <=79 ) {
+            System.out.println( "C" );
+        } else if( a >= 60 && a <= 69 ) {
+            System.out.println( "D" );
+        } else {
+            System.out.println("F" );
+        }
+    }   
+}
